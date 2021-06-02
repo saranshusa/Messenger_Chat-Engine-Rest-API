d7f6f4d5430c7a2c9263cbaa7ce4e10df1437fa1
Saransh Gupta <82395600+saranshusa@users.noreply.github.com>
2021-06-03 05:15:20 +0530

    Chat Engine API connected

---
+++ chat_app/src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from "react-chat-engine";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="0ef9569d-9943-4c53-a2a1-5bb7e3144dff"
+      userName="saranshgupta"
+      userSecret="123456"
+    />
+  );
+};
+
+export default App;
