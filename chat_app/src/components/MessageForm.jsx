5bd58ab74bbac408d42e0310581dc0cbc28bf7c9
Saransh Gupta <82395600+saranshusa@users.noreply.github.com>
2021-06-13 21:11:10 +0530

    Update MessageForm.jsx

--- chat_app/src/components/MessageForm.jsx
+++ chat_app/src/components/MessageForm.jsx
@@ -1,5 +1,25 @@
-const MessageForm = () => {
-  return <div>MessageForm</div>;
+import { useState } from "react";
+
+const MessageForm = () => { 
+  const [value, setValue] = useState('');
+
+  const handleSubmit = () => {
+    
+  }
+  const handleChange = () => {
+
+  }
+
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+    <input
+    className="message-input"
+    placeholder="Send a message..."
+    value={value}
+    onChange={handleChange}
+    />    
+    </form>
+  );
 };
 
 export default MessageForm;
