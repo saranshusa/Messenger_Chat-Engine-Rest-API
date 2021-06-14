340db50524ab31b9905f82db99875cb3299d4054
Saransh Gupta <82395600+saranshusa@users.noreply.github.com>
2021-06-14 15:27:44 +0530

    Create LoginForm.jsx

---
+++ chat_app/src/components/LoginForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import axios from "axios";
+
+const LoginForm = () => {
+    const [userName, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    }
+
+    return (
+        <div className='wraper'>
+            <div className='form'>
+                <h1 className='title'>Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type='text' value={userName} onChange={(e) => setUsername(e.target.value)} className='input' placeholder='Username' required />
+                    <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} className='input' placeholder='Password' required />
+                    <div align='center'>
+                        <button type='submit' className='button'>
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
