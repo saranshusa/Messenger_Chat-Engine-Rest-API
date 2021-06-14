570b1252ebaea4c04743cd3beb11740f5a408a88
Saransh Gupta <82395600+saranshusa@users.noreply.github.com>
2021-06-14 15:27:48 +0530

    Update ChatFeed.jsx

--- chat_app/src/components/ChatFeed.jsx
+++ chat_app/src/components/ChatFeed.jsx
@@ -7,6 +7,19 @@ const ChatFeed = (props) => {
 
   const chat = chats && chats[activeChat];
 
+  const renderReadReceipts = (message, isMyMessage) => {
+    return chat.people.map((person, index) => person.last_read === message.id && (
+      <div
+        key={`read_${index}`}
+        className='read-receipt'
+        style={{
+          float: isMyMessage ? 'right' : 'left',
+          backgroundImage: `url(${person?.person?.avatar})`
+        }}
+      />
+    ))
+  }
+ 
   const renderMessages = () => {
     const keys = Object.keys(messages);
 
@@ -34,7 +47,7 @@ const ChatFeed = (props) => {
               marginLeft: isMyMessage ? "0px" : "60px",
             }}
           >
-            read-receipts
+            {renderReadReceipts(message, isMyMessage)}
           </div>
         </div>
       );
